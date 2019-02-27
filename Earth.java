f45f3ff5e29fb48026b94307bc397e6b50d0775a
Abdelfattah Altaeb <42971313+aftiyah94@users.noreply.github.com>
2019-02-27 10:46:42 +0200

    first commit

---
+++ Earth.java
@@ -0,0 +1,19 @@
+public class Earth {
+    public static void main(String[] args) {
+        Human aftiyah;
+        aftiyah = new Human();
+        aftiyah.name="Abdalfayyah Altaeb";
+        aftiyah.age=24;
+        aftiyah.Color="Green";
+        aftiyah.higthInInches=105;
+        aftiyah.speak();
+
+        Human abdo;
+        abdo = new Human();
+        abdo.name="Abdalrahman Altaeb";
+        abdo.age=21;
+        abdo.Color="brown";
+        abdo.higthInInches=103;
+        abdo.speak();
+    }
+}
