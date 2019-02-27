41bca14b22f07e0f29dd0b13041dc28e6d0a463f
Abdelfattah Altaeb <42971313+aftiyah94@users.noreply.github.com>
2019-02-27 11:29:29 +0200

    Change The Constructor Using fields

--- Earth.java
+++ Earth.java
@@ -1,10 +1,11 @@
 public class Earth {
     public static void main(String[] args) {
-        Human human1=new Human();
-        Human human2=new Human();
-        Human human3=new Human();
-        human1.speak();
-        human2.speak();
-        human3.speak();
-    }
+
+            Human human1=new Human("Abdalfattah Altaeb", 24, 105, "Green");
+            Human human2=new Human("Abdalrahman Altaeb", 21,103, "Broun");
+            Human human3=new Human("Mazen Takiyta", 32, 99, "Black");
+            human1.speak();
+            human2.speak();
+            human3.speak();
+        }
 }
