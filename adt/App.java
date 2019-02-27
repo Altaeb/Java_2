f8b62228cafff2402ca895765e1832f5bcf47f8a
Abdelfattah Altaeb <42971313+aftiyah94@users.noreply.github.com>
2019-02-28 00:04:53 +0200

    I added new folder ds.stack

--- adt/App.java
+++ adt/App.java
@@ -1,5 +1,5 @@
 package adt;
-
+/*
 public class App {
     public static void main(String[] args) {
 
@@ -12,3 +12,4 @@ public class App {
 
     }
 }
+*/
---
+++ ds/stack/App.java
@@ -0,0 +1,18 @@
+package ds.stack;
+
+public class App {
+    public static void main(String[] args) {
+        Stack theStack=new Stack(10);
+        theStack.push(20);
+        theStack.push(40);
+        theStack.push(60);
+        theStack.push(80);
+
+        while (!theStack.isEmpty()) {
+            long value = theStack.pop();
+            System.out.println(value);
+
+        }
+
+    }
+}
